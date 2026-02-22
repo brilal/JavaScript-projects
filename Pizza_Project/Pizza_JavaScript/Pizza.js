75967f924788cd4f608bfd9b4963e725204cb76a
brilal <brijlal40@gmail.com>
2026-02-22 14:06:59 -0500

    Add pizza ordering functionality with toppings

--- Pizza_Project/Pizza_JavaScript/Pizza.js
+++ Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -1,1 +1,30 @@
 
+// Run when the page is loaded
+window.addEventListener("DOMContentLoaded", function () {
+    const orderButton = document.getElementById("orderButton");
+    const sizeSelect = document.getElementById("size");
+    const outputDiv = document.getElementById("output");
+
+    orderButton.addEventListener("click", function () {
+        const size = sizeSelect.value;
+
+        const toppingCheckboxes = document.querySelectorAll("#pizza-form input[type='checkbox']");
+        let toppings = [];
+
+        toppingCheckboxes.forEach(function (box) {
+            if (box.checked) {
+                toppings.push(box.value);
+            }
+        });
+
+        let message = "You ordered a " + size + " pizza";
+
+        if (toppings.length > 0) {
+            message += " with " + toppings.join(", ") + ".";
+        } else {
+            message += " with no extra toppings.";
+        }
+
+        outputDiv.textContent = message;
+    });
+});
